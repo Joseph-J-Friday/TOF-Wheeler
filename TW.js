7fa0a6ed3646e73194fd1e5c03db801a414294c1
Joseph-J-Friday <83015160+Joseph-J-Friday@users.noreply.github.com>
2021-08-06 15:32:44 +0100

    Update TW.js

--- TW.js
+++ TW.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 7777;
 
 var ROOMS = [];
 
-app.use(express.static('__dirname'));
+app.use(express.static(__dirname));
 app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname,'/index.html'));
 });
